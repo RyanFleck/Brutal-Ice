9ee74f869ffc5e91174bf8243bc9906497eee3b0
Ryan Fleck <ryan.fleck@protonmail.com>
2018-11-09 23:55:51 -0500

    Late night commit.

--- src/main.js
+++ src/main.js
@@ -23,25 +23,44 @@ const app = new Application(256, 240, {
 function game() {
     console.log('running...');
 
-    app.renderer.resize(window.innerWidth, window.innerHeight);
+    resize(app.renderer);
     app.view.style.display = 'block';
     app.autoResize = true;
     document.getElementById('loading').remove();
     document.body.appendChild(app.view);
 
     window.addEventListener('resize', () => {
-        app.renderer.resize(window.innerWidth, window.innerHeight);
+        resize(app.renderer);
     }, false);
 
     loader
-        .add('sprites/player-v1.png')
+        .add('pv1', 'sprites/player-v1.png')
+        .on('progress', handleProgress)
         .load(setup);
 }
 
+/*
+ * Setup runs after all resources have been loaded as textures.
+ */
 function setup() {
-    const sprite = new Sprite(resources['sprites/player-v1.png'].texture);
+    const sprite = new Sprite(resources.pv1.texture);
+    console.log(`
+    Sprite size: ${sprite.width},${sprite.height}
+    `);
+    sprite.pivot.set(sprite.width / 2, sprite.height / 2);
+    sprite.x = window.innerWidth / 8;
+    sprite.y = window.innerHeight / 8;
     app.stage.addChild(sprite);
     console.log('gotime.');
+}
+
+function resize(renderer) {
+    console.log(`Resizing to ${window.innerWidth}, ${window.innerHeight}.`);
+    renderer.resize(window.innerWidth, window.innerHeight);
+}
+
+function handleProgress(iloader, resource) {
+    console.log(`Loading resources [${iloader.progress}%]: ${resource.url}`);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
